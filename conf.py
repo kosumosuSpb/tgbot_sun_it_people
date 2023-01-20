950a64298eba1a3b426e3684ab701b122b9dc640
kosumosu <kasmas@yandex.ru>
2023-01-20 17:29:47 +0300

    * добавлена возможность изменять разсписание запуска задач (по сути одной задачи - праздники присылать) * также стало можно вызвать праздники командой * добавил ещё логгеров * теперь расписание выводится в консоль для контроля * команда /help выводит список команд

--- conf.py
+++ conf.py
@@ -2,17 +2,10 @@ TOKEN = ""  # токен, полученный при регистрации
 HOLIDAYS_SOURCE = 'https://kakoysegodnyaprazdnik.ru/'
 HOROSCOPE_SOURCE = ''
 
-# CELERY CONFIG
-# celery_broker_url = 'redis://localhost:6379/0'
-# celery_result_backend = 'redis://localhost:6379/0'
-# celery_broker_url = 'redis://redis:6379/0'
-# celery_result_backend = 'redis://redis:6379/1'
-
-# celery_task_serializer = 'json'
-# celery_result_serializer = 'json'
-# celery_accept_content = ['json']
-# celery_timezone = 'Europe/Moscow'
-# celery_enable_utc = True
+# SCHEDULE
+HOUR = 10
+MINUTE = 0
+SECOND = 0
 
 # GREETINGS
 # TODO: вынести это в отдельные файлы в папке conf, например, и парсить при запуске
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import datetime as dt
 from time import sleep
 from threading import Thread
 from typing import Callable
+import re
 #
 import telebot
 from scheduler import Scheduler
@@ -15,30 +16,55 @@ from utils import *
 logging.config.dictConfig(LOG_CONFIG)
 logger = logging.getLogger('GreeTG_Bot')
 
-
 bot = telebot.TeleBot(TOKEN)  # создание экземпляра класса TeleBot и передача токена ему
+
+# SERVICE VARIABLES
 run = True
+schedule = None
+tasks_thread = None
 
 
-# Обработчики команд
+# КОМАНДЫ
+
 @bot.message_handler(commands=['start'], chat_types=["private", "group", "supergroup"])
-def start(message: telebot.types.Message):
+def cmd_start(message: telebot.types.Message):
     """Обработчик команды /start"""
-    task = set_schedule(send_morning_message, chat_id=message.chat.id)
-    Thread(target=start_tasks, args=(task,), name='Periodically_tasks').start()
-    logger.info('Расписание запущено')
-    bot.send_message(message.chat.id, 'Расписание запущено')
+    logger.debug(f'Got command: {message.text}')
+    task = schedule or set_schedule(send_morning_message, chat_id=message.chat.id)
+
+    global run
+    global tasks_thread
+
+    if tasks_thread:
+        logger.debug('Перезагрузка треда с расписанием')
+        run = False
+        while tasks_thread.is_alive():
+            sleep(1)
+        run = True
+
+    logger.debug('Запуск треда с расписанием')
+    tasks_thread = Thread(target=start_tasks, args=(task,), name='Periodically_tasks').start()
+    # logger.info('Расписание запущено')
+    bot.send_message(message.chat.id, 'Расписание запущено. '
+                                      'Для получения списка команд введите /help')
 
 
 @bot.message_handler(commands=['help'], chat_types=["private", "group", "supergroup"])
-def help_message(message: telebot.types.Message):
+def cmd_help_message(message: telebot.types.Message):
     """Обработчик команды /help"""
-    bot.send_message(message.chat.id, 'тест для вывода на команду /help')
+    logger.debug(f'Got command: {message.text}')
+    bot.send_message(message.chat.id, '☮️ /start - запуск расписания (по-умолчанию в 10:00:00)\n'
+                                      '☮️ /stop - остановка расписания\n'
+                                      '☮️ /holidays - вывести праздники прямо сейчас\n'
+                                      '☮️ /stop_bot - остановка бота (права только у владельца)\n'
+                                      '☮️ /set_schedule HH:MM:SS - установка нового расписания\n'
+                                      '☮️ /help - это сообщение')
 
 
 @bot.message_handler(commands=['stop'], chat_types=["group", "supergroup"], is_chat_admin=True)
-def stop(message: telebot.types.Message):
+def cmd_stop(message: telebot.types.Message):
     """Обработчик команды /stop"""
+    logger.debug(f'Got command: {message.text}')
     global run
     run = False
     bot.send_message(message.chat.id, f'Расписание остановлено пользователем '
@@ -47,8 +73,9 @@ def stop(message: telebot.types.Message):
 
 
 @bot.message_handler(commands=['stop'], chat_types=["private"])
-def stop(message: telebot.types.Message):
+def cmd_stop(message: telebot.types.Message):
     """Обработчик команды /stop"""
+    logger.debug(f'Got command: {message.text}')
     global run
     run = False
     bot.send_message(message.chat.id, f'Расписание остановлено пользователем '
@@ -57,8 +84,9 @@ def stop(message: telebot.types.Message):
 
 
 @bot.message_handler(commands=['stop_bot'], chat_types=["private", "group", "supergroup"], is_chat_admin=True)
-def stop_bot(message: telebot.types.Message):
+def cmd_stop_bot(message: telebot.types.Message):
     """Обработчик команды /stop_bot"""
+    logger.debug(f'Got command: {message.text}')
 
     # TODO: переделать на нормальный фильтр
     if message.from_user.username != 'kosumosu':
@@ -70,6 +98,32 @@ def stop_bot(message: telebot.types.Message):
     bot.stop_bot()
     logger.info('Бот остановлен')
 
+
+@bot.message_handler(commands=['holidays'], chat_types=["private", "group", "supergroup"])
+def cmd_get_holidays(message: telebot.types.Message):
+    """Обработчик команды /holidays"""
+    logger.debug(f'Got command: {message.text}')
+    send_morning_message(message.chat.id)
+
+
+@bot.message_handler(commands=['set_schedule'], chat_types=["private", "group", "supergroup"])
+def cmd_set_schedule(message: telebot.types.Message):
+    """Обработчик команды /set_schedule"""
+    logger.debug(f'Got command: {message.text}')
+
+    try:
+        hour, minute, second = map(int, re.findall('[0-9]{2}', message.text))
+    except ValueError:
+        bot.reply_to(message, 'Не верный формат времени! Должен быть: HH:MM:SS, например: 10:00:00')
+        return
+
+    logger.debug(f'Запущено изменение расписания. Новое время: {hour}:{minute}:{second}')
+    global schedule
+    schedule = set_schedule(send_morning_message, hour=hour, minute=minute, second=second)
+    cmd_start(message)
+
+
+# СОБЫТИЯ
 
 @bot.message_handler(content_types=["new_chat_members"], chat_types=["group", "supergroup"])
 def new_member(message: telebot.types.Message):
@@ -88,27 +142,24 @@ def left_member(message: telebot.types.Message):
     bot.send_message(message.chat.id, f"{member.first_name} (@{member.username}) покинул(а) чат о_О")
 
 
-def send_message_to_group(text: str, chat_id: int):
-    """отправка сообщения в группу"""
-    bot.send_message(chat_id, text)
-
-
 # TASKS
 
 def start_tasks(schedule: Scheduler):
-    global run
     logger.debug(f'Расписание запущено в {dt.datetime.now().ctime()}')
+    logger.debug(schedule)
     while run:
         schedule.exec_jobs()
         sleep(1)
-
     logger.debug('Расписание остановлено')
 
 
 # TODO: сделать задание расписания через сообщение
-def set_schedule(task: Callable, *, hour=10, minute=0, second=0, **kwargs):
+def set_schedule(task: Callable, *, hour=None, minute=None, second=None, **kwargs):
     """Создаёт расписание и возвращает экземпляр класса Scheduler"""
     schedule = Scheduler()
+    hour = hour or HOUR
+    minute = minute or MINUTE
+    second = second or SECOND
     schedule.daily(dt.time(hour=hour, minute=minute, second=second), task, kwargs=kwargs)
     return schedule
 
@@ -121,15 +172,16 @@ def get_str_holidays():
     return dict_to_format_string(all_events)
 
 
-def send_morning_message(chat_id):
+def send_morning_message(chat_id, with_greetings=True):
     greetings = generate_greeting()
     holidays = get_str_holidays()
 
-    send_message_to_group(greetings, chat_id)
-    send_message_to_group(holidays, chat_id)
+    if with_greetings:
+        bot.send_message(chat_id, greetings)
+    bot.send_message(chat_id, holidays)
 
 
 if __name__ == '__main__':
     logger.info('Start bot')
-    bot.polling(none_stop=True)  # запуск с параметром "не останавливать работу"
+    bot.polling(none_stop=True)
     logger.info('Bot has been stopped')
